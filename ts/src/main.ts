314d990f216d723f3c8e0f0e51b752eeeb7d9107
Asfandyar Khan <a_asfandyar1@hotmail.com>
2024-12-25 01:16:21 +0000

    Learning generics

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -3,13 +3,15 @@ type Pizza = {
   name: string
   price: number
 }
+let nextPizzaId = 1;
 
 const menu: Pizza[] = [
-  {id: 1, name: "Margherita", price: 8},
-  {id: 2, name: "Pepperoni", price: 10},
-  {id: 3, name: "Hawaiian", price: 5},
-  {id: 4, name: 'Veggie', price: 9},
+  {id: nextPizzaId++, name: "Margherita", price: 8},
+  {id: nextPizzaId++, name: "Pepperoni", price: 10},
+  {id: nextPizzaId++, name: "Hawaiian", price: 5},
+  {id: nextPizzaId++, name: 'Veggie', price: 9},
 ]
+
 
 type Order = {
   id: number
@@ -22,11 +24,16 @@ let nextOrderId = 1;
 let orderQueue: Order[] = [];
 
 
-function addNewPizza(pizzaObj: {id: number, name: string, price: number}){
-  menu.push(pizzaObj);
+function addNewPizza(pizzaObj: Omit<Pizza, 'id'>): Pizza {
+  const newPizza: Pizza = {
+    id: nextPizzaId++,
+    ...pizzaObj
+  }
+  menu.push(newPizza);
+  return newPizza
 }
 
-function placeOrder(pizzaName: string) {
+function placeOrder(pizzaName: string): Order | undefined {
   const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
   if (!selectedPizza) {
     console.error(`${pizzaName} does not exist in the menu`)
@@ -38,7 +45,7 @@ function placeOrder(pizzaName: string) {
   return newOrder
 }
 
-function completeOrder(orderId: number) {
+function completeOrder(orderId: number): Order | Error {
   const order = orderQueue.find(order => order.id === orderId)
   if (!order) {
     console.error(`${order} was not found in the orderQueue`)
@@ -49,17 +56,21 @@ function completeOrder(orderId: number) {
 }
 
 
-function getPizzaDetail(identifier: string | number){
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
   if (typeof identifier === 'string') {
     return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
-  } else {
+  } 
+  else if (typeof identifier === 'number') {
     return menu.find(order => order.id === identifier)
+  }
+  else {
+    throw new TypeError('Parameter `identifier` must be either string or a number')
   }
 }
 
-addNewPizza({ id: 5, name: 'Chicken Bacon Ranch', price: 12 })
-addNewPizza({ id: 6, name: 'BBQ Chicken', price: 12 })
-addNewPizza({ id: 7, name: 'Spicy Sausage', price: 12 })
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 })
+addNewPizza({ name: 'BBQ Chicken', price: 12 })
+addNewPizza({ name: 'Spicy Sausage', price: 12 })
 
 placeOrder('Chicken Bacon Ranch')
 completeOrder(1)
@@ -68,3 +79,40 @@ console.log('Menu:', menu)
 console.log('Cash in register:', cashInRegister)
 console.log('Order queue:', orderQueue)
 
+
+// type User = {
+//   id: number
+//   username: string
+//   role: 'member' | 'contributor' | 'admin'
+// }
+
+// type UpdatedUser = Partial<User>
+
+// let nextUserId: number = 1
+
+// const users: User[] = [
+//   { id: nextUserId, username: 'john_doe', role: 'member' },
+//   { id: nextUserId, username: 'jane_smith', role: 'contributor' },
+// ]
+
+// function updateUser(id: number, updates: UpdatedUser): void {
+//   const person = users.find(user => user.id == id)
+//   if (!person) {
+//     console.error('something went wrong')
+//     return
+//   }
+//   Object.assign(person, updates)
+// }
+
+// function addNewUser(newUser: Omit<User, 'id'>): User {
+//   const user: User = {
+//     id: nextUserId++,
+//     ...newUser
+//   }
+//   users.push(user)
+//   return user
+// }
+
+// addNewUser({ username: 'joe_schmoe', role: 'member' })
+
+// console.log(users)
