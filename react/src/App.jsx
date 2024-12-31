9916d8584844265faac171eefb1e61d2c15d33d3
Asfandyar Khan <a_asfandyar1@hotmail.com>
2024-12-31 09:37:17 +0000

    removed react file

--- react/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  const ninjaTurtles = ['Leonardo', 'Donatello', 'Michelangelo', 'Raphael']
-  const map1 = ninjaTurtles.map((turtle) => <p>{turtle}</p>)
-  return (
-    <main>
-      {map1}
-    </main>
-  )
-}
